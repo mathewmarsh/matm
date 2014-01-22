8c4d578d7257b518c16d4e1172272c946c19f801
Mat Marsh <mathew@marsh.im>
2014-01-22 09:46:15 -0500

    Initial commit of simple batch file that outputs Windows environment variables to disc.

---
+++ dumpEnvVars.bat
@@ -0,0 +1,4 @@
+# pipes all environment variables to file in %USERPROFILE%
+echo Writing environment variables to : %USERPFILE%\environmentVariables.txt
+set > %USERPROFILE%\environmentVariables.txt
+echo Done!
