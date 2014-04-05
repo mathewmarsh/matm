434615ddbeaaca910dab5154d922bc3fe91e7d85
Mat Marsh <marsh87@gmail.com>
2014-04-05 12:24:46 -0400

    Deleted duplicate bat

--- dumpEnvVars.bat
+++
@@ -1,4 +0,0 @@
-# pipes all environment variables to file in %USERPROFILE%
-echo Writing environment variables to : %USERPFILE%\environmentVariables.txt
-set > %USERPROFILE%\environmentVariables.txt
-echo Done!
